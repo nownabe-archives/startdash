2db3f918686a9c9e48002935365933939d49df61
nownabe <nownabe@gmail.com>
2016-12-21 05:52:19 +0900

    :sparkles: Add anyenv cookbook

--- common_cookbooks/anyenv/default.rb
+++ common_cookbooks/anyenv/default.rb
@@ -1,1 +1,17 @@
-puts "anyenv"
+package "git"
+
+git "#{home}/.anyenv" do
+  user username
+  repository "https://github.com/riywo/anyenv"
+end
+
+remote_file "#{home}/.zsh.d/anyenv.sh" do
+  owner username
+  group username
+  source "files/home/user/.zsh.d/anyenv.sh"
+end
+
+git "#{home}/.anyenv/plugins/anyenv-update" do
+  user username
+  repository "https://github.com/znz/anyenv-update.git"
+end
